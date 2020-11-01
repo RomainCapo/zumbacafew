ade4bfbd4824e2c67119bc1f143f39cd59e4aa8d
JonasFreibur <jonas.freibs@gmail.com>
2020-11-01 22:27:36 +0100

    add stop words wip

--- frontend/wordcloud.js
+++ frontend/wordcloud.js
@@ -2,10 +2,11 @@
     d3.json("http://localhost:8080/api/artists/termfrequency").then(data => {
 
         let myWords = data;
+        let mostFrequentWord = Math.max.apply(Math, myWords.map(function (o) { return o.count; }));
 
-        var margin = {top: 0, right: 40, bottom: 34, left: 40},
-            width = 1200,
-            height = 600;
+        var margin = { top: 0, right: 40, bottom: 34, left: 40 },
+            width = 800,
+            height = 800;
 
         var svg = d3.select("#svgwordcloud").append("svg")
             .attr("width", width + margin.left + margin.right)
@@ -14,23 +15,28 @@
             .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
 
+        var xScale = d3.scaleLinear()
+            .range([0, width]);
+
         var layout = d3.layout.cloud()
             .size([width, height])
-            .words(myWords.map(function (d) { return { text: d._id, size: d.count / 100 }; }))
-            .padding(0)
+            .words(myWords.map(function (d) { return { text: d._id, size: (d.count / mostFrequentWord) }; }))
+            .padding(2)
             .rotate(function () { return ~~(Math.random() * 2) * 90; })
-            .fontSize(function (d) { return d.size; })
+            .fontSize(function (d) { return d.size * width / 3; })
             .on("end", draw);
         layout.start();
 
         function draw(words) {
             svg.append("g")
+                .attr("width", width)
+                .attr("height", height)
                 .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
                 .selectAll("text")
                 .data(words)
                 .enter().append("text")
-                .style("font-size", function (d) { return d.size; })
-                .style("fill", "#69b3a2")
+                .style("font-size", function (d) { return d.size + "px"; })
+                .style("fill", "#000000")
                 .attr("text-anchor", "middle")
                 .style("font-family", "Impact")
                 .attr("transform", function (d) {
