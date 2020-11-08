326cef1c9425dbbe1d8f7ffbf6031917b1681469
Romain Capocasale <romain.capocasale99@gmail.com>
2020-11-08 18:26:41 +0100

    refactor nav

--- vue-zumbacafew/src/components/layout/Header.vue
+++ vue-zumbacafew/src/components/layout/Header.vue
@@ -1,26 +1,54 @@
 <template>
   <nav class="navbar navbar-expand-lg">
     <div class="container">
-      <img src="res/img/logo.png" alt="logo">
-      <h1 class="text-left">Zumba Cafew</h1>
+      <!--<img src="res/img/logo.png" alt="logo" />-->
+      <span class="navbar-brand">Zumba Cafew</span>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Beeswarm</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Histogramme</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Nuage de mot</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'Header'
-}
+  name: "Header",
+};
 </script>
 
 <style scoped>
-nav{
-    background-color: rgb(255, 255, 255);
-    border-bottom: 1px solid rgb(0, 178, 99);
+nav {
+  background-color: rgb(255, 255, 255);
+  border-bottom: 1px solid rgb(0, 178, 99);
+}
+
+nav a{
+  font-size: 24px;
+}
+
+nav .navbar-brand{
+  font-size: 36px;
+  margin-right: 60px;
+}
+
+nav img{
+  margin-right: 5px;
 }
 
 nav h1 {
-    position: absolute;
-    margin-left: 80px;
+  position: absolute;
+  margin-left: 80px;
 }
 </style>
