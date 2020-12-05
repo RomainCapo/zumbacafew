5868c926aca9332b65ec6fbc502fa76152d6c95a
Moulin Vincent <vincentmoulin47@gmail.com>
2020-12-05 16:38:17 +0100

    add linechart, set label axis

--- vue-zumbacafew/src/components/charts/Linechart.vue
+++ vue-zumbacafew/src/components/charts/Linechart.vue
@@ -10,32 +10,54 @@ export default {
     mounted() {
         var chart = this.$refs.chart;
         var ctx = chart.getContext("2d");
+        var data = {
+          labels: ["2012", "2013", "2014", "2015", "2016"],
+          datasets: [
+            {
+              label: "Word",
+              data: [5, 4, 5, 12, 15],
+              backgroundColor: "#00C663",
+              borderColor: "#00C663",
+              fill: false,
+              lineTension: 0,
+              radius: 6
+            }
+          ]
+        };
+
+        var options = {
+          responsive: true,
+          legend: {
+            display: true,
+            position: "bottom",
+            labels: {
+              fontColor: "#333",
+              fontSize: 16
+            }
+          },
+          scales: {
+            yAxes: [{
+              ticks: {
+                min: 0
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "Nombre de mots"
+              }
+            }],
+            xAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: "Année"
+              }
+            }]
+          },
+        };
+
         new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
+          type: "line",
+          data: data,
+          options: options
         });
     },
     props: {
