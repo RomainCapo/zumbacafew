aa9dc4d39f0a869f1617790e943bf425a85985ff
RomainCapo <romain.capocasale99@gmail.com>
2021-05-16 23:03:16 +0200

    update axios

--- vue-zumbacafew/src/services/api/Artists.js
+++ vue-zumbacafew/src/services/api/Artists.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = process.env.VUE_APP_BACKENDURL;
+//axios.defaults.baseURL = process.env.VUE_APP_BACKENDURL;
 
 export default {
   name: "ArtistsAPI",
