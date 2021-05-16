1caf483248e1c646c4ee114d50a7043bb971e8e7
RomainCapo <romain.capocasale99@gmail.com>
2021-05-16 21:24:59 +0200

    update nginx.conf

--- vue-zumbacafew/src/main.js
+++ vue-zumbacafew/src/main.js
@@ -1,14 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-//import VueGtag from "vue-gtag-next";
-
-/*createApp(App).use(VueGtag, {
-    id: 'G-Z8815G01K9',
-    linker:{
-      domain: ['zumbacafew.ch']
-    }
-}).mount('#app')
-*/
 
 createApp(App).mount('#app')
 
